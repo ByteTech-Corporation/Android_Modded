c35063f66999202243b0c9cb6d67198a4f6855a0
Boonk3312 <theboonk7@hotmail.com>
2024-01-20 12:43:15 -0800

    Create filedefinitions.hpp
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

---
+++ ANDROID_MODDED/bin/filedefinitions.hpp
@@ -0,0 +1,29 @@
+package com.bytetech.androidmodded.filedefs;
+
+int main() {
+
+  char(*trace_files_0) == ".tr0";
+  char(*trace_files_1) == ".tr1";
+  char(*debug_apps) == ".debugapp";
+  char(*debugger_helper) == ".dbgh";
+
+  // Character fonts file types
+  
+  char(*ttf_font_type) == ".ttf";
+  char(*cstmfft_font_type) == ".cstmfft";
+
+  // Text files
+
+  char(*log_files_file_type) == ".log";
+  char(*text_file_normal_0) == ".txt0";
+  char(*readme_files) == ".rdm";
+  char(*customized_text_files) == ".ctf";
+  char(*md_files_software_open_source_foss_git_code) == ".md";
+
+  // Java files
+
+  char(*executable_jar_file) == ".jar";
+  char(*javafx_executable) == ".jfxe";
+  char(*javafx_customized_executable) == ".cjfxe";
+  char(*javafx_admin_executable_file) == ".ajfxe";
+}
